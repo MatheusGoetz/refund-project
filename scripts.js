11d8e7e2182ee6c8074de3cf922c389417140903
Matheus Goetz <goetz.dev@gmail.com>
2025-05-25 19:58:36 -0300

    Capturando valor do input amount e validando a remoção de letras

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Selecionando os elementos do formulário
+const amount = document.getElementById("amount");
+
+
+amount.oninput = () => {
+  let value = amount.value.replace(/\D/g, '')
+  
+  amount.value = value
+}
